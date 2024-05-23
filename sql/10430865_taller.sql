746d65520ed6a95e99b69a76e3d848c04ead7f74
Ivan <ivan.ruicos@educa.jcyl.es>
2024-05-23 09:25:41 +0200

    Cambios base de datos

--- sql/10430865_taller.sql
+++ sql/10430865_taller.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 02-05-2024 a las 16:11:06
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Servidor: PMYSQL175.dns-servicio.com:3306
+-- Tiempo de generación: 23-05-2024 a las 09:24:26
+-- Versión del servidor: 8.0.35
+-- Versión de PHP: 8.2.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,28 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `taller`
+-- Base de datos: `10430865_taller`
 --
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `averias`
---
-
-CREATE TABLE `averias` (
-  `id_averia` int(11) NOT NULL,
-  `tipo` varchar(100) DEFAULT NULL,
-  `tiempo` int(11) DEFAULT NULL,
-  `precio` decimal(10,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `averias`
---
-
-INSERT INTO `averias` (`id_averia`, `tipo`, `tiempo`, `precio`) VALUES
-(1, 'Cambio de aceite', 2, 200.00);
 
 -- --------------------------------------------------------
 
@@ -48,9 +28,9 @@ INSERT INTO `averias` (`id_averia`, `tipo`, `tiempo`, `precio`) VALUES
 --
 
 CREATE TABLE `citas` (
-  `id_cita` int(11) NOT NULL,
-  `DNI` varchar(10) DEFAULT NULL,
-  `descripcion` text DEFAULT NULL,
+  `id_cita` int NOT NULL,
+  `DNI` varchar(10) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `hora` time DEFAULT NULL,
   `fecha` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -58,8 +38,9 @@ CREATE TABLE `citas` (
 -- Volcado de datos para la tabla `citas`
 --
 
-INSERT INTO `citas` (`id_cita`, `DNI`, `descripcion`, `fecha`) VALUES
-(3, '70265782T', 'Primera cita', '2024-04-10');
+INSERT INTO `citas` (`id_cita`, `DNI`, `hora`, `fecha`) VALUES
+(55, '1234567C', '08:00:00', '2024-05-16'),
+(60, '70265782T', '16:00:00', '2024-05-28');
 
 -- --------------------------------------------------------
 
@@ -68,9 +49,9 @@ INSERT INTO `citas` (`id_cita`, `DNI`, `descripcion`, `fecha`) VALUES
 --
 
 CREATE TABLE `clientes` (
-  `DNI` varchar(10) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `DNI` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  `nombre` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -79,7 +60,15 @@ CREATE TABLE `clientes` (
 
 INSERT INTO `clientes` (`DNI`, `nombre`, `password`) VALUES
 ('11111111A', 'admin', '$2y$10$dpNBpBCLKE27uCqTLdIR0enBmA6ddnmijS9KwWn/TX8AGxUPDspfG'),
-('70265782T', 'ivan', '$2y$10$q/jbTpmOsvalUVTHAG22RO0pW6DpTNNxfe9ypNW0v919At.6q7RO6');
+('12345678L', 'Usuario', '$2y$10$hF558BQ6MEePxgNZ0V62a.PuZNV85UK9TMgcGim/tFxJTtS3NelyG'),
+('12345678P', 'Pedro Sánchez', '$2y$10$3n4Ffu0YnpXrN7g4OKIM/uVnV/Gj5YlRQbWM0MOhXGo5O8nwZW40G'),
+('12345678Z', 'Alvaro', '$2y$10$Bz0CQYd9FbN.jspy/NgNFef41newmWggpkZGsZndZKysFUnXfa7Ne'),
+('1234567C', 'Pedro Sánchez', '$2y$10$./NNnAqEx91NBwhXGTWS2uNCGL9y6RhMF9JSivfDa8B3pdGZzVX7O'),
+('1234567D', 'Leñadora', '$2y$10$d30LV/WiOqLswcHEUV/6ruK4Ye6Ow1cTSA/KyyN07kBqpGz25gXAy'),
+('33333333C', 'admin 3', '$2y$10$w8XqmVIJlj2oZNdJOts6e.EBtz47DnSkwbqAlHDfyl/dVAp5nzB1y'),
+('70265782T', 'ivan', '$2y$10$q/jbTpmOsvalUVTHAG22RO0pW6DpTNNxfe9ypNW0v919At.6q7RO6'),
+('70274733G', 'María García Conde', '$2y$10$5yXioIgnPCp02YLN0SNsXuJyEzfzbStPPIo/XQXfL5Jx30ZTyGEUi'),
+('98765432P', 'Calla', '$2y$10$zoCIfEcNfH1S3fIIOO9oOuc4fAeIXuoicRpbBerETbxskxqNVuP3.');
 
 -- --------------------------------------------------------
 
@@ -88,10 +77,10 @@ INSERT INTO `clientes` (`DNI`, `nombre`, `password`) VALUES
 --
 
 CREATE TABLE `intervencion` (
-  `id_i` int(11) NOT NULL,
-  `id_v` int(11) DEFAULT NULL,
+  `id_i` int NOT NULL,
+  `id_v` int DEFAULT NULL,
   `fecha` date DEFAULT NULL,
-  `descripcion` text DEFAULT NULL,
+  `descripcion` text COLLATE utf8mb4_general_ci,
   `precio` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -105,46 +94,29 @@ INSERT INTO `intervencion` (`id_i`, `id_v`, `fecha`, `descripcion`, `precio`) VA
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `intervencion_averias`
---
-
-CREATE TABLE `intervencion_averias` (
-  `id_i` int(11) NOT NULL,
-  `id_averia` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `vehiculos`
 --
 
 CREATE TABLE `vehiculos` (
-  `id_v` int(11) NOT NULL,
-  `DNI` varchar(10) DEFAULT NULL,
-  `matricula` varchar(15) DEFAULT NULL,
-  `marca` varchar(50) DEFAULT NULL,
-  `kilometros` int(11) DEFAULT NULL,
-  `año` int(11) DEFAULT NULL,
-  `modelo` varchar(50) DEFAULT NULL
+  `id_v` int NOT NULL,
+  `DNI` varchar(10) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `matricula` varchar(15) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `marca` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `kilometros` int DEFAULT NULL,
+  `anio` int DEFAULT NULL,
+  `modelo` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `vehiculos`
 --
 
-INSERT INTO `vehiculos` (`id_v`, `DNI`, `matricula`, `marca`, `kilometros`, `año`, `modelo`) VALUES
+INSERT INTO `vehiculos` (`id_v`, `DNI`, `matricula`, `marca`, `kilometros`, `anio`, `modelo`) VALUES
 (3, '70265782T', '2291GVC', 'BMW', 180000, 2010, 'Serie 1');
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `averias`
---
-ALTER TABLE `averias`
-  ADD PRIMARY KEY (`id_averia`);
 
 --
 -- Indices de la tabla `citas`
@@ -167,13 +139,6 @@ ALTER TABLE `intervencion`
   ADD KEY `id_v` (`id_v`);
 
 --
--- Indices de la tabla `intervencion_averias`
---
-ALTER TABLE `intervencion_averias`
-  ADD PRIMARY KEY (`id_i`,`id_averia`),
-  ADD KEY `id_averia` (`id_averia`);
-
---
 -- Indices de la tabla `vehiculos`
 --
 ALTER TABLE `vehiculos`
@@ -185,28 +150,22 @@ ALTER TABLE `vehiculos`
 --
 
 --
--- AUTO_INCREMENT de la tabla `averias`
---
-ALTER TABLE `averias`
-  MODIFY `id_averia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT de la tabla `citas`
 --
 ALTER TABLE `citas`
-  MODIFY `id_cita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_cita` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT de la tabla `intervencion`
 --
 ALTER TABLE `intervencion`
-  MODIFY `id_i` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_i` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `vehiculos`
 --
 ALTER TABLE `vehiculos`
-  MODIFY `id_v` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_v` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
@@ -223,13 +182,6 @@ ALTER TABLE `citas`
 --
 ALTER TABLE `intervencion`
   ADD CONSTRAINT `intervencion_ibfk_1` FOREIGN KEY (`id_v`) REFERENCES `vehiculos` (`id_v`);
-
---
--- Filtros para la tabla `intervencion_averias`
---
-ALTER TABLE `intervencion_averias`
-  ADD CONSTRAINT `intervencion_averias_ibfk_1` FOREIGN KEY (`id_i`) REFERENCES `intervencion` (`id_i`),
-  ADD CONSTRAINT `intervencion_averias_ibfk_2` FOREIGN KEY (`id_averia`) REFERENCES `averias` (`id_averia`);
 
 --
 -- Filtros para la tabla `vehiculos`
